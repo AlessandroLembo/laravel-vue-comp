8e27e72d0b996493ed01e200a4a571a1f8c419bf
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-03-20 17:19:22 +0100

    Create structure migrate

--- database/migrations/2023_03_20_145425_create_videogames_table.php
+++ database/migrations/2023_03_20_145425_create_videogames_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('videogames', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 50)->unique();
+            $table->text('description')->nullable();
+            $table->string('image')->nullable();
+            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
